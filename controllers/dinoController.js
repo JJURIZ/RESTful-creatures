5170164baa299477fe3cc140a07905243ea03748
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-17 11:20:37 -0500

    [controller, view] reading json file, display dino

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,8 +1,22 @@
+const { raw } = require('express');
+const fs = require('fs')
+
 const dinoRouter = require('express').Router();
 
 
 dinoRouter.get('/', (req, res) => {
-    res.render('dinosaurs/index')
+    const rawDinos = fs.readFileSync('dinosaurs.json') //will read in text from this file
+    const dinos = JSON.parse(rawDinos)
+
+    res.render('dinosaurs/index', { dinos })
+})
+
+dinoRouter.get('/:id', (req, res) => {
+    const rawDinos = fs.readFileSync('dinosaurs.json') //will read in text from this file
+    const dinos = JSON.parse(rawDinos)
+    const id = parseInt(req.params.id) - 1
+    const dino = dinos[id]
+    res.render('dinosaurs/show', { dino })
 })
 
 
--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const layouts = require('express-ejs-layouts')
 
 // additional setup
 app.set('view engine', 'ejs');
-
+app.use(layouts);
 // our routes
 app.get('/', (req, res) => {
-    res.send('hello I am alive!')
+    res.send('ugh')
 })
 
 app.use('/dinosaurs', dinoRouter);
-app.use(layouts);
+
 
 //listen
 app.listen(8000, () => {
