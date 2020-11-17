209788926ff0b729fec85c1fa29ded09d77df474
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-17 16:48:01 -0500

    [controllers, views] Created Edit, Delete for dino

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,37 +1,88 @@
-const { raw } = require('express');
-const fs = require('fs')
+const { raw } = require("express");
+const fs = require("fs");
 
-const dinoRouter = require('express').Router();
+const dinoRouter = require("express").Router();
 
+dinoRouter.get("/", (req, res) => {
+  const rawDinos = fs.readFileSync("dinosaurs.json"); //will read in text from this file
+  const dinos = JSON.parse(rawDinos);
 
-dinoRouter.get('/', (req, res) => {
-    const rawDinos = fs.readFileSync('dinosaurs.json') //will read in text from this file
-    const dinos = JSON.parse(rawDinos)
-
-    res.render('dinosaurs/index', { dinos })
-})
+  res.render("dinosaurs/index", { dinos });
+});
 
 // new has to be above show, or else it will think 'new' is an id value
-dinoRouter.get('/new', (req, res) => {
-    res.render('dinosaurs/new');
+dinoRouter.get("/new", (req, res) => {
+  res.render("dinosaurs/new");
+});
+
+dinoRouter.get("/:id", (req, res) => {
+  const rawDinos = fs.readFileSync("dinosaurs.json"); //will read in text from this file
+  const dinos = JSON.parse(rawDinos);
+  const id = parseInt(req.params.id) - 1;
+  const dino = dinos[id];
+  res.render("dinosaurs/show", { dino });
+});
+
+dinoRouter.post("/", (req, res) => {
+  const newDino = req.body;
+  const rawDinos = fs.readFileSync("dinosaurs.json");
+  const dinos = JSON.parse(rawDinos);
+  dinos.push(newDino);
+
+  fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinos));
+  res.redirect("/dinosaurs");
+});
+
+dinoRouter.get("/search/:searchTerm", (req, res) => {
+  const newDino = req.body;
+  const rawDinos = fs.readFileSync("dinosaurs.json");
+  const dinos = JSON.parse(rawDinos);
+  const searchTerm = req.params.searchTerm;
+
+  //note that the details of the search are up to you
+  // do partial matches count? do we look at the type as well as the name proprty?
+  // this is a crude, 1st draft kind of search
+  const filteredDinos = dinos.filter(
+    (dino) => dino.name.toLowerCase() === searchTerm.toLowerCase()
+  );
+
+  console.log(searchTerm);
+  res.render("dinosaurs/index", { dinos: filteredDinos });
+});
+
+dinoRouter.get('/edit/:id', function(req, res) {
+    const rawDinos = fs.readFileSync('./dinosaurs.json');
+    const dinos = JSON.parse(rawDinos);
+    res.render('dinosaurs/edit', { dino: dinos[req.params.id], dinoId: req.params.id })
 })
 
-dinoRouter.get('/:id', (req, res) => {
-    const rawDinos = fs.readFileSync('dinosaurs.json') //will read in text from this file
-    const dinos = JSON.parse(rawDinos)
-    const id = parseInt(req.params.id) - 1
-    const dino = dinos[id]
-    res.render('dinosaurs/show', { dino })
-})
+dinoRouter.delete("/:id", function (req, res) {
+  console.log("-----DELETE route----");
+  const rawDinos = fs.readFileSync("./dinosaurs.json");
+  const dinos = JSON.parse(rawDinos);
 
-dinoRouter.post('/', (req, res) => {
-    const newDino = req.body
-    const rawDinos = fs.readFileSync('dinosaurs.json')
-    const dinos = JSON.parse(rawDinos)
-    dinos.push(newDino)
+  // Remove the deleted dino from the dino array
+  dinos.splice(req.params.id, 1);
 
-    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
-    res.redirect('/dinosaurs');
-})
+  //Save the new dinos to the json file
+  fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinos));
+
+  // Redirect t the GET /dinsaurs route (index)
+  res.redirect("/dinosaurs");
+});
+
+dinoRouter.put("/:id", function (req, res) {
+  console.log("____ PUT route____");
+  const rawDinos = fs.readFileSync("./dinosaurs.json");
+  const dinos = JSON.parse(rawDinos);
+
+  // Re-assign the name and type of the dinosaur fields to be edited
+  const dinoObject = dinos[req.params.id];
+  dinoObject.name = req.body.name;
+  dinoObject.type = req.body.type;
+
+  fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinos));
+  res.redirect("/dinosaurs");
+});
 
 module.exports = dinoRouter;
--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const express = require('express')
 const app = express()
 const dinoRouter = require('./controllers/dinoController');
 const cryptidRouter = require('./controllers/cryptidController');
+const methodOverride = require('method-override')
 const layouts = require('express-ejs-layouts')
 
 // additional setup
 app.set('view engine', 'ejs');
 app.use(layouts);
-app.use(express.urlencoded({extended: false})) // gives access to the 'body' part of form request/POST
+app.use(express.urlencoded({extended: false})) 
+app.use(methodOverride('_method'))
+
+// gives access to the 'body' part of form request/POST
 // our routes
 
 app.get('/', (req, res) => {
     res.send('ugh')
 })
 
+// Controllers used
 app.use('/dinosaurs', dinoRouter);
 app.use('/cryptids', cryptidRouter);
+
 
 //listen
 app.listen(8000, () => {
