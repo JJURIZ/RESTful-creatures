32da4417da7e9141c3fa4b660b5fba39359eabf6
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-17 12:40:55 -0500

    [controller, views] Cryptid view and controllers

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -11,6 +11,11 @@ dinoRouter.get('/', (req, res) => {
     res.render('dinosaurs/index', { dinos })
 })
 
+// new has to be above show, or else it will think 'new' is an id value
+dinoRouter.get('/new', (req, res) => {
+    res.render('dinosaurs/new');
+})
+
 dinoRouter.get('/:id', (req, res) => {
     const rawDinos = fs.readFileSync('dinosaurs.json') //will read in text from this file
     const dinos = JSON.parse(rawDinos)
@@ -19,5 +24,14 @@ dinoRouter.get('/:id', (req, res) => {
     res.render('dinosaurs/show', { dino })
 })
 
+dinoRouter.post('/', (req, res) => {
+    const newDino = req.body
+    const rawDinos = fs.readFileSync('dinosaurs.json')
+    const dinos = JSON.parse(rawDinos)
+    dinos.push(newDino)
+
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
+    res.redirect('/dinosaurs');
+})
 
 module.exports = dinoRouter;
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 //require stuff
-const app = require('express')()
-const dinoRouter = require('./controllers/dinoController')
+const express = require('express')
+const app = express()
+const dinoRouter = require('./controllers/dinoController');
+const cryptidRouter = require('./controllers/cryptidController');
 const layouts = require('express-ejs-layouts')
 
 // additional setup
 app.set('view engine', 'ejs');
 app.use(layouts);
+app.use(express.urlencoded({extended: false})) // gives access to the 'body' part of form request/POST
 // our routes
+
 app.get('/', (req, res) => {
     res.send('ugh')
 })
 
 app.use('/dinosaurs', dinoRouter);
-
+app.use('/cryptids', cryptidRouter);
 
 //listen
 app.listen(8000, () => {
